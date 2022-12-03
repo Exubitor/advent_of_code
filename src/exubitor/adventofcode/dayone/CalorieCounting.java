103f52c54f183414abdcbf78b441793171d148d1
Emre Aral <emre.aral.2002@googlemail.com>
2022-12-03 17:29:53 +0100

    add logic to determine the elf with the most calories

---
+++ src/exubitor/adventofcode/dayone/CalorieCounting.java
@@ -0,0 +1,57 @@
+package exubitor.adventofcode.dayone;
+
+import exubitor.adventofcode.util.MyFileInput;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CalorieCounting {
+    private final List<String> inputAsStrings;
+    private final List<List<Integer>> elfInventories;
+    private final List<Integer> elfInventoriesCalorieSums;
+
+    public CalorieCounting() {
+        File input = new File("src/exubitor/adventofcode/dayone/resource/input.txt");
+        inputAsStrings = MyFileInput.parseInputToStringList(input);
+        elfInventories = new ArrayList<>();
+        parseElfInventories();
+        elfInventoriesCalorieSums = new ArrayList<>();
+        addSumOfCaloriesForEachElfInventory();
+        Collections.sort(elfInventoriesCalorieSums);
+    }
+
+    private void parseElfInventories() {
+        List<Integer> currentElfInventory = new ArrayList<>();
+        for (String s : inputAsStrings) {
+            if (s.equals("")) {
+                elfInventories.add(currentElfInventory);
+                currentElfInventory = new ArrayList<>();
+            } else {
+                currentElfInventory.add(Integer.parseInt(s));
+            }
+        }
+        elfInventories.add(currentElfInventory); //adds the last elfInventory to the list
+    }
+
+    private void addSumOfCaloriesForEachElfInventory() {
+        for (List<Integer> elfInventory : elfInventories) {
+            int sumOfCaloriesInInventory = 0;
+            for (int calorieCount : elfInventory) {
+                sumOfCaloriesInInventory += calorieCount;
+            }
+            elfInventoriesCalorieSums.add(sumOfCaloriesInInventory);
+        }
+    }
+
+    public int getHighestCalorieCount() {
+        //being sorted in the constructor
+        return elfInventoriesCalorieSums.get(elfInventoriesCalorieSums.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        CalorieCounting cc = new CalorieCounting();
+        System.out.println(cc.getHighestCalorieCount());
+    }
+}
