1027671711379c55b00e0fc5187cc32c85f31d70
Emre Aral <emre.aral.2002@googlemail.com>
2022-12-04 14:29:29 +0100

    remove unnecessary imports

--- src/exubitor/adventofcode/daythree/RucksackReorganization.java
+++ src/exubitor/adventofcode/daythree/RucksackReorganization.java
@@ -4,9 +4,7 @@ import exubitor.adventofcode.util.MyFileInput;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class RucksackReorganization {
     private final List<String> rucksacks;
